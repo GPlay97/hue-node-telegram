aea5e6fdaa98d32e7c61691f69d2313b23f22a83
GPlay97 <GPlay97@users.noreply.github.com>
2018-06-30 23:01:00 +0200

    add basic structure and first initialization of server

---
+++ index.js
@@ -0,0 +1,57 @@
+/**
+ * @file index.js
+ * @author GPlay97
+ * @description
+ * The server main file, where server will be initialized to listen for requests and starting the Telegram Bot.
+ */
+const express = require('express'),
+    app = express(),
+    errors = require('./errors.json'),
+    config = require('./config.json'),
+    bodyParser = require('body-parser'),
+    hue = require('./hue.js'),
+    telegram = require('./telegram.js');
+
+// JSON parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// set default headers
+app.use((req, res, next) => {
+    res.contentType('application/json');
+    res.setHeader('Access-Control-Allow-Origin', ((req.get('origin') == null) || req.get('origin') == 'null') ? '*' : req.get('origin') || '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
+// the request routes
+app.get('/bridges', hue.findBridges);
+
+// requested route doesn't exist
+app.use((req, res) => {
+    res.status(404).json({
+        error: {
+            code: errors.UNKNWON_ROUTE.code,
+            message: errors.UNKNWON_ROUTE.message
+        }
+    });
+});
+
+// error handling (Arrow function not working here?)
+app.use(function onError(err, req, res, next) {
+    res.status(500).json({
+        error: {
+            code: errors.INTERNAL_ERROR.code,
+            message: errors.INTERNAL_ERROR.message
+        }
+    });
+    next(err);
+});
+
+// start the Telegram Bot
+telegram.startBot();
+
+// initialize the server
+app.listen(config.PORT, () => console.log('Server started on port ', config.PORT));
