629993cdc008c5e7fddecde22d7a04cae6afa9a9
GPlay97 <GPlay97@users.noreply.github.com>
2018-07-05 21:56:03 +0200

    bugfix route not found; add session and login handling to protect routes

--- src/index.js
+++ src/index.js
@@ -6,11 +6,23 @@
  */
 const express = require('express'),
     app = express(),
+    session = require('express-session'),
     errors = require('./errors.json'),
     srv_config = require('./../config.json'),
     bodyParser = require('body-parser'),
+    auth = require('./auth.js'),
     hue = require('./hue.js'),
     telegram = require('./telegram.js');
+
+// session handling
+app.use(session({
+    secret: srv_config.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false // only for http
+    }
+}));
 
 // JSON parsing
 app.use(bodyParser.json());
@@ -26,7 +38,21 @@ app.use((req, res, next) => {
     next();
 });
 
+// check authentication
+app.use((req, res, next) => {
+    // if not on login route, check first if authenticated
+    if (req.url === '/login' || req.session.authenticated) return next();
+    // not authorized
+    return res.status(401).json({
+        error: {
+            code: errors.UNAUTHORIZED.code,
+            message: errors.UNAUTHORIZED.message
+        }
+    });
+});
+
 // the request routes
+app.post('/login', auth.validatePassword);
 app.get('/bridges', hue.findBridges);
 app.get('/config', hue.getConfig);
 app.get('/lights', hue.getLights);
@@ -37,8 +63,8 @@ app.post('/off', hue.turnOff);
 app.use((req, res) => {
     res.status(404).json({
         error: {
-            code: errors.UNKNWON_ROUTE.code,
-            message: errors.UNKNWON_ROUTE.message
+            code: errors.UNKNOWN_ROUTE.code,
+            message: errors.UNKNOWN_ROUTE.message
         }
     });
 });
